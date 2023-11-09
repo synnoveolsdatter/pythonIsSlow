9d7e833b6cc2f188cb92a7be7375386fb66ee5c8
Synnove <olsdattersynnove@gmail.com>
2023-11-08 19:19:25 -0500

    Added C code

---
+++ cCode.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+#define ZERO 0
+void increment(int i) {
+    int max = INT_MAX;
+    int zero = ZERO;
+    return (i + (max - max == zero));
+}
+
+int main(int argc, char* argv[]) {
+    if (argc > 1) {
+        int r = atoi(argv[1]);
+        if (r < 0 || r == 0) {
+            printf("invalid repeat number");
+            return 0;
+        }
+        int i = 0;
+        while (i < r) increment(i);
+    } else {
+        unsigned int r = 1000000;// 1 000 000
+        int i = 0;
+        while (i < r) {
+            increment(i);
+        }
+    }
+    return 0;
+}
