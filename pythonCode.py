2e301fcb26532b83dabbb8e78963137771eaffe7
Synnove <olsdattersynnove@gmail.com>
2023-11-08 19:31:44 -0500

    Added Python Code

---
+++ pythonCode.py
@@ -0,0 +1,25 @@
+import sys
+
+ZERO = 0
+def increment(i):
+    maximum = sys.maxint
+    zero = ZERO
+    return (i + int(maximum - maximum == zero))
+
+def main():
+    if (sys.argc > 1) {
+        r = sys.argv[1]
+        if r < 0:
+            sys.exit()
+        i = 0
+        while i < r:
+            increment(i)
+        print(i)
+
+    else:
+        r = 1000000# 1 000 000
+        i = 0
+        while i < r:
+            increment(i)
+        print(i)
+
